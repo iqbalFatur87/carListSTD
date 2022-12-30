4798be7d3a74ff02ef599ab668826b61e74c5ac5
--add <--add>
2022-12-30 17:26:25 +0700

    add 2 case

--- QUEUE/main.cpp
+++ QUEUE/main.cpp
@@ -11,10 +11,12 @@ int main()
     createQueue(Q);
     do
     {
-        cout << "1. Enqueue" << endl;
-        cout << "2. Dequeue" << endl;
-        cout << "3. Print" << endl;
-        cout << "4. Exit" << endl;
+        cout << "1. Masukkan Antrian Mobil " << endl;
+        cout << "2. Hapus Antrian Mobil" << endl;
+        cout << "3. Cetak Antrian Mobil" << endl;
+        cout << "4. Cari Antrian Mobil berdasarkan plat nomor" << endl;
+        cout << "5. Hitung Antrian Mobil" << endl;
+        cout << "6. Exit" << endl;
         cout << "Pilihan: ";
         cin >> pilihan;
         cout << endl;
@@ -50,6 +52,32 @@ int main()
             break;
         case 4:
             cout<<endl;
+            cout << "Masukkan Plat No: ";
+            cin >> platNo;
+            cout << endl;
+            P = Q.head;
+            while (P != NULL)
+            {
+                if (P->platNo == platNo)
+                {
+                    cout << "Plat No: " << P->platNo << endl;
+                    cout << endl;
+                    break;
+                } else {
+                    P = P->next;
+                }
+            }
+            if (P == NULL)
+            {
+                cout << "Plat No tidak ditemukan" << endl;
+            }
+            break;
+        case 5:
+            cout<<endl;
+            cout << "Jumlah Antrian: " << countElmQ(Q) << endl;
+            break;
+        case 6:
+            cout<<endl;
             cout << "Terima kasih Telah menggunakan program Queue" << endl;
             break;
         default:
@@ -57,5 +85,5 @@ int main()
             cout << "Pilihan tidak ada" << endl;
             break;
         }
-    } while (pilihan != 4);
+    } while (pilihan != 6);
 }
